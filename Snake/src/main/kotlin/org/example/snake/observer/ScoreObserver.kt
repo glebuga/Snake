a68d450e05f902d99de012dc0a4f13154e1bb88e
glebuga <133004556+glebuga@users.noreply.github.com>
2024-04-12 13:58:47 +0300

    create project2

--- Snake/src/main/kotlin/org/example/snake/observer/ScoreObserver.kt
+++
@@ -1,7 +0,0 @@
-class ScoreObserver(private val gameModel: GameModel) : Observer {
-    override fun update() {
-        val score = gameModel.score
-        // Здесь можно добавить логику обновления пользовательского интерфейса для отображения текущего счета
-        println("Score updated: $score")
-    }
-}
